2f6f49dea49914825f5dbe47ffccdad0ebe0e983
preenachandi <120080294+preenachandi@users.noreply.github.com>
2025-02-08 20:24:46 +0530

    images

--- src/components/KioskFeatures.jsx
+++ src/components/KioskFeatures.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-
+import Features from "../assets/effortless-kiosk-deployement-image.webp";
+import Feature from "../assets/customized-interface-image.webp";
+import Features2 from "../assets/power-up-protection-image.webp";
+import Features3 from "../assets/secure-app-ecosystem.webp";
+import Features4 from "../assets/secure-app-ecosystem.webp";
 
 
 const KioskFeatures = () => {
@@ -10,31 +14,31 @@ const KioskFeatures = () => {
       title: "Effortless kiosk deployment & management",
       description:
         "Deploy kiosk-enabled devices straight out of the box. Flash a custom Android Enterprise, Samsung Knox or ROM with Hexnode App on the devices by collaborating with OEM vendors who provide specially configured ROMs.",
-        image: "../src/assets/effortless-kiosk-deployement-image.webp",
+        image: Features,
     },
     {
       title: "Customized interface for brand visibility",
       description:
         "Create a locked-down environment with customized interface. Maximize brand visibility and leave a lasting impression by showcasing products, services and key messages directly to users through the customized interface.",
-        image: "../src/assets/customized-interface-image.webp",
+        image: Feature,
     },
     {
       title: "What more can you do with Hexnode kiosk?",
       description:
         "Ensure compliance of your devices by remotely deploying the latest OS version while the device is still in kiosk mode. Prevent your data from falling into the wrong hands even in case of device loss/theft with the various remote management features.",
-        image: "../src/assets/power-up-protection-image.webp",
+        image: Features2,
     },
     {
       title: "Secure and update your app ecosystem",
       description:
         "Streamline the deployment and management on apps on your kiosk devices. Save your time and effort, ensure security and improve your efficiency using Hexnode’s silent app installation and update features.",
-        image: "../src/assets/secure-app-ecosystem.webp",
+        image: Features3,
     },
     {
       title: "Provide an easy-to-use interface for end-users",
       description:
         "Give your end-users the power to control their devices without overwhelming them with options. An intuitive interface to let them access only the essential settings they need. Make it easy for them to unlock the full potential of your devices hassle-free.",
-        image: "../src/assets/easy-to-use-interface-image.webp",
+        image:Features4,
     },
   ];
 
--- src/components/RecognitionSection.jsx
+++ src/components/RecognitionSection.jsx
@@ -1,3 +1,6 @@
+import RecognitionSections from "../assets/idc.webp"
+import RecognitionSections2 from "../assets/gartner.webp"
+import RecognitionSections3 from "../assets/forrester.webp"
 function RecognitionSection() {
   return (
     <div className="text-white py-10 bg-[#1A1C2B] md:py-10">
@@ -5,7 +8,7 @@ function RecognitionSection() {
 
         {/* IDC */}
         <div className="flex flex-col items-center md:items-start border-b md:border-b-0 md:border-r border-gray-700 px-4 pb-6 md:pb-0 transform transition duration-300 hover:-translate-y-2">
-          <img src="../src/assets/idc.webp" alt="IDC" className="h-6 mb-2" />
+          <img src={RecognitionSections} alt="IDC" className="h-6 mb-2" />
           <p className="text-sm md:text-base text-gray-400 leading-relaxed">
             Hexnode is listed as a leader and a major player in IDC MarketScape UEM Vendors Assessment Reports 2024.
           </p>
@@ -13,7 +16,7 @@ function RecognitionSection() {
 
         {/* Gartner */}
         <div className="flex flex-col items-center md:items-start border-b md:border-b-0 md:border-r border-gray-700 px-4 pb-6 md:pb-0 transform transition duration-300 hover:-translate-y-2">
-          <img src="../src/assets/gartner.webp" alt="Gartner" className="h-6 mb-2" />
+          <img src={RecognitionSections2} alt="Gartner" className="h-6 mb-2" />
           <p className="text-sm md:text-base text-gray-400 leading-relaxed">
             Hexnode was recognized in the 2023 Gartner® Market Guide for Unified Endpoint Management Tools.
           </p>
@@ -21,7 +24,7 @@ function RecognitionSection() {
 
         {/* Forrester */}
         <div className="flex flex-col items-center md:items-start px-4 transform transition duration-300 hover:-translate-y-2">
-          <img src="../src/assets/forrester.webp" alt="Forrester" className="h-6 mb-2" />
+          <img src={RecognitionSections3} alt="Forrester" className="h-6 mb-2" />
           <p className="text-sm md:text-base text-gray-400 leading-relaxed">
             Forrester includes Hexnode as a Notable vendor in The Unified Endpoint Management Landscape, Q3 2023.
           </p>
